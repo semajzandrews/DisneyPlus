273325761e8f6c5b144dfa8b62e48bd626ece24c
Semaj Andrews <semajzandrews@gmail.com>
2022-04-04 19:28:05 -0400

    Project Creation. Initial Commit

---
+++ DisneyPlus/DisneyPlus/DisneyPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisneyPlusApp.swift
+//  DisneyPlus
+//
+//  Created by Semaj Andrews on 4/4/22.
+//
+
+import SwiftUI
+
+@main
+struct DisneyPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
